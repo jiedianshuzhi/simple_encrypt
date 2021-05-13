5d9e1072c81a420495ac33b368eaab83998beb6a
hzy <18670314023@163.com>
2021-05-13 12:12:58 +0800

    fix

---
+++ lib/simple_encrypt.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative "simple_encrypt/version"
+
+module SimpleEncrypt
+  class Error < StandardError; end
+  RULE = ['F', 'T', 'X', 'K', 'L', 'E', 'R', 'B', 'G', 'J']
+
+  def self.encrypt data
+    data.to_i.to_s.split('').map{|f| RULE[f.to_i]}.join('')
+  end
+
+  def self.decrypt data
+    data.to_s.upcase.split('').map{|f| RULE.index(f) || '!'}.join('')
+  end
+
+end
